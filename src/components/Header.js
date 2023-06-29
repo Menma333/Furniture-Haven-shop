6245c18db91aacdfb2b1036800f5cf650f6ed438
Shvager Artur <menma0n@gmail.com>
2023-06-29 22:31:20 +0200

    Creating functionality for deleting from the cart

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,30 +1,47 @@
-import React, { useState } from 'react'
+import React, { useState } from 'react';
 import { BiCartDownload } from "react-icons/bi";
 import Order from './Order';
 
+const showOrders = (props) => {
+  return (
+    <div>
+      {props.orders.map(el => (
+        <Order key={el.id} item={el} />
+      ))}
+    </div>
+  );
+};
+
+const showNothing = () => {
+  return (
+    <div className='empty'>
+      <h2>Cart is empty</h2>
+    </div>
+  );
+};
+
 export default function Header(props) {
-  let [cartOpen, setCartOpen] = useState(false)
+  let [cartOpen, setCartOpen] = useState(false);
 
   return (
     <header className='header'>
       <div>
-        <span className='logo'>House Staff</span>
+        <span className='logo'>Furniture Haven</span>
         <ul className='nav'>
           <li>About us</li>
           <li>Contacts</li>
           <li>Cabinet</li>
         </ul>
-        <BiCartDownload onClick={() => setCartOpen(cartOpen = !cartOpen)} className={`shop-cart-button ${cartOpen && 'active'}`} />
+        <BiCartDownload onClick={() => setCartOpen(!cartOpen)} className={`shop-cart-button ${cartOpen && 'active'}`} />
 
         {cartOpen && (
           <div className='shop-cart'>
-            {props.orders.map(el => {
-              <Order key={el.id} item={el} />
-            })}
+            {props.orders.length > 0 ?
+              showOrders(props) : showNothing()}
           </div>
         )}
       </div>
       <div className='presentation'></div>
     </header>
-  )
+  );
 }
--- src/components/Order.js
+++ src/components/Order.js
@@ -4,7 +4,9 @@ export class Order extends Component {
   render() {
     return (
       <div className='item'>
-
+        <img src={"./img/" + this.props.item.img} alt={''} />
+        <h2>{this.props.item.title}</h2>
+        <b>{this.props.item.price}$</b>
       </div>
     )
   }
