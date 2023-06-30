0937624f16d7096ee62081992f80d4778b380846
Shvager Artur <menma0n@gmail.com>
2023-06-30 12:19:03 +0200

    Create a grand total calculation

---
+++ src/components/Header.jsx
@@ -0,0 +1,52 @@
+import React, { useState } from "react";
+import { BiCartDownload } from "react-icons/bi";
+import Order from "./Order";
+
+const showOrders = (props) => {
+  let summa = 0;
+  props.orders.forEach((el) => (summa += Number.parseFloat(el.price))); // Вычисление суммы
+  return (
+    <div>
+      {props.orders.map((el) => (
+        <Order onDelete={props.onDelete} key={el.id} item={el} />
+      ))}
+      <p className="summa">Sum: {summa}</p>
+    </div>
+  );
+};
+
+const showNothing = () => {
+  return (
+    <div className="empty">
+      <h2>Cart is empty</h2>
+    </div>
+  );
+};
+
+export default function Header(props) {
+  let [cartOpen, setCartOpen] = useState(false);
+
+  return (
+    <header className="header">
+      <div>
+        <span className="logo">Furniture Haven</span>
+        <ul className="nav">
+          <li>About us</li>
+          <li>Contacts</li>
+          <li>Cabinet</li>
+        </ul>
+        <BiCartDownload
+          onClick={() => setCartOpen(!cartOpen)}
+          className={`shop-cart-button ${cartOpen && "active"}`}
+        />
+
+        {cartOpen && (
+          <div className="shop-cart">
+            {props.orders.length > 0 ? showOrders(props) : showNothing()}
+          </div>
+        )}
+      </div>
+      <div className="presentation"></div>
+    </header>
+  );
+}
