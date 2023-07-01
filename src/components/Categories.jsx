c4e2fc613440799709fd64c64c4f7d1d74d7d992
Shvager Artur <menma0n@gmail.com>
2023-07-01 10:46:45 +0200

    I create a functionality to view full information about all products on the site

--- src/components/Categories.jsx
+++ src/components/Categories.jsx
@@ -41,7 +41,9 @@ export class Categories extends Component {
     return (
       <div className="categories">
         {this.state.categories.map((el) => (
-          <div key={el.key}>{el.name}</div>
+          <div key={el.key} onClick={() => this.props.chooseCategory(el.key)}>
+            {el.name}
+          </div>
         ))}
       </div>
     );
